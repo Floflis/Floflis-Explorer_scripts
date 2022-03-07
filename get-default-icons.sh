448cc5ba2a3fdec01b6b350be94cffe39f9b0d53
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-07 19:41:15 -0300

    Detect Desktop Environment and get the current icon theme name/path

--- get-default-icons.sh
+++ get-default-icons.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -e
 
+if [ "$(echo $XDG_CURRENT_DESKTOP)" = *"GNOME" ]; then
+   gsettings_path="org.gnome.desktop.interface"
+   gsettings_key="icon-theme"
+fi
 
+if [ "$(echo $XDG_CURRENT_DESKTOP)" = "X-Cinnamon" ]; then
+   gsettings_path="org.cinnamon.desktop.interface"
+   gsettings_key="icon-theme"
+fi
+
+#gsettings get $gsettings_path $key | tr -d "'"
+
+#echo "$(gsettings get $gsettings_path $key | tr -d "'")"
+
+gsettings get $gsettings_path $gsettings_key | tr -d "'"
