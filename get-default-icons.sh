a1408ebf32b1f1c0f9c4da3e7d78fd0bb24dcfe0
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-03-07 20:21:48 -0300

    Credits in get-default-icons.sh

--- get-default-icons.sh
+++ get-default-icons.sh
@@ -4,6 +4,7 @@ if [ "$(echo $XDG_CURRENT_DESKTOP)" = *"GNOME" ]; then
    gsettings_path="org.gnome.desktop.interface"
    gsettings_key="icon-theme"
 fi
+#- credits: https://itsfoss.com/find-desktop-environment/
 
 if [ "$(echo $XDG_CURRENT_DESKTOP)" = "X-Cinnamon" ]; then
    gsettings_path="org.cinnamon.desktop.interface"
@@ -15,3 +16,4 @@ fi
 #echo "$(gsettings get $gsettings_path $key | tr -d "'")"
 
 gsettings get $gsettings_path $gsettings_key | tr -d "'"
+#- credits: https://stackoverflow.com/a/29788100/5623661
